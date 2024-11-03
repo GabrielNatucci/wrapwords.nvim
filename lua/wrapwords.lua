e0a436419d49cb6ade4fad3ca1ac474b1ff690ea
GabrielNatucci <gabriel.natuccirusso@hotmail.com>
2024-11-03 19:34:52 -0300

    removing the "teste" usercommand

--- lua/wrapwords.lua
+++ lua/wrapwords.lua
@@ -1,11 +1,9 @@
 local mainlogic = require("plugin.main")
 
 local function setup()
-    vim.api.nvim_create_user_command("Teste", mainlogic.teste, {})
     vim.api.nvim_create_user_command("Wrapwordsdoublequotes", mainlogic.wrap_word_in_quotes, {})
     vim.api.nvim_create_user_command("Wrapwordssinglequotes", mainlogic.wrap_word_in_single_quotes, {})
     vim.api.nvim_create_user_command("Wrapwordsparentheses",   mainlogic.wrap_word_in_parentheses, {})
-
 end
 
 return { setup = setup }
